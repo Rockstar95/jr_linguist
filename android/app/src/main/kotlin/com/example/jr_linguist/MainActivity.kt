0ebbdb9f58fd1095220c5ffb6dbb4988ced1d04d
Dishant <agrawaldishant29@gmail.com>
2022-12-08 12:13:37 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/jr_linguist/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.jr_linguist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home_screen/home_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../../utils/SizeConfig.dart';
+import '../../utils/styles.dart';
+import '../common/components/app_bar.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Styles.background,
+      body: Column(
+        children: [
+          MyAppBar(title: "Home", color: Colors.white, backbtnVisible: false,),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: MySize.size20!),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Center(
+                    child: Text("Home"),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
